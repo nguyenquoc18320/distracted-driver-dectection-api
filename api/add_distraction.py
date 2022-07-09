a62df5f98ba9d6eb7466659f52bdde294e80ceb4
nguyenquoc18320 <nguyenquoc18320@gmail.com>
2022-07-09 09:56:32 +0700

    add threshold, distraction statistic

--- api/add_distraction.py
+++ api/add_distraction.py
@@ -1,20 +1,30 @@
 from datetime import datetime
 from unittest import result
 from fastapi import HTTPException
+from auth.auth_handler import decodeJWT
 from main import app
 from fastapi import FastAPI, File, UploadFile, Depends,Form
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from ML_model.predict import predict
+from auth.auth_bearer import JWTBearer
 from services.predict import add_distrac, add_no_person
 from pydantic import BaseModel
 import os
 
 from services.total_images import add_num_image
+from services.user import get_account_by_userid
 class New_Distraction(BaseModel):
     userid: int
 
 @app.post("/add-distraction")
-def add_distraction(userid: int = Form(...), img: UploadFile = File(...)):
+def add_distraction(img: UploadFile = File(...),  token: str = Depends(JWTBearer())):
+
+    userid = decodeJWT(token)['user_id']
+    account = get_account_by_userid(userid)
+
+    if account is None or account.status==False:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     original_image = Image.open(img.file)
     # original_image.show()
     print('save')
@@ -22,17 +32,19 @@ def add_distraction(userid: int = Form(...), img: UploadFile = File(...)):
     if(os.path.exists(dest_folder) == False): 
             os.mkdir(dest_folder)
             
-
-    original_image.save(dest_folder + "/" + img.filename) 
+    now =datetime.now()
+    img_name=  str(userid) + "_" + now.strftime("%Y_%m_%d_%H_%M_%S_%f") + "." + img.filename.split('.')[-1]
+    # print(img_name)
+    original_image.save(dest_folder + "/" + img_name) 
     bbox, class_name, prob = predict(original_image)
 
     #not detect person
     if bbox is None:
         print('h1')
-        result_distrac = add_no_person(class_name, dest_folder + "/" + img.filename, userid)
+        result_distrac = add_no_person(class_name, dest_folder + "/" +img_name, userid)
     else:
         print('h2')
-        result_distrac = add_distrac(class_name, dest_folder + "/" + img.filename,userid)
+        result_distrac = add_distrac(class_name, dest_folder + "/" + img_name,userid)
     if result_distrac is None:
             print('err1')
             raise HTTPException(status_code=500, detail="Internal Server Error")
--- api/get_distraction.py
+++ api/get_distraction.py
@@ -7,29 +7,67 @@ from services.user import *
 from services.distraction import *
 
 
-
 # get total distraction in a day
 @app.get('/get-total-distraction')
-def getTotalDistraction(date: date_type, token: str = Depends(JWTBearer())):
-    #get user
-    userid = decodeJWT(token)['user_id']
+def getTotalDistraction(userid: int, date: date_type, token: str = Depends(JWTBearer())):
+    # get user
+
+    current_userid = decodeJWT(token)['user_id']
+
+    if current_userid != userid and check_admin_role_by_token(token) == False:
+        raise HTTPException(status_code=401, detail="Unauthorized")
     # user = get_user_by_id(userid)
 
     # if user is None:
     #     raise HTTPException(status_code=401, detail="Unauthorized")
 
-    #get account
+    # get account
     account = get_account_by_userid(userid)
 
-    if account is None or account.status==False:
+    if account is None or account.status == False:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
     total = get_total_distraction_by_user(userid, date)
 
     if total is None:
         raise HTTPException(status_code=500, detail="Internal Server Error")
-    
+
     return {"total": total}
-    
-        
-    
+
+
+# GET ALL number of distraction (admin role)
+@app.get("/get-total-distraction-for-all")
+def getTotalDistractionForAll(date: date_type, token: str = Depends(JWTBearer())):
+    if check_admin_role_by_token(token) == False:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    num_distraction = get_total_distraction(date)
+
+    if num_distraction is None:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    return {"total": num_distraction}
+
+
+#get statistic of distractions, num_distractions are arranged desc,
+# using pagination 
+@app.get('/get-statistic-distraction')
+def getStatisticDistraction(date: date_type, page: int, items_per_page: int,
+                             token: str = Depends(JWTBearer())):
+    if check_admin_role_by_token(token) == False:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    if page < 1:
+        return {"data": [],
+                "total_pages": 0}
+
+    result, num_pages = get_num_distraction_for_each_user(
+        date, page, items_per_page)
+
+    #error processing
+    if result is None:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    return {"data": result,
+            "page": page,
+            "total_pages": num_pages}
