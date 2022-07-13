42982ced1aaad553b84afbd1a8d492803f89b87e
nguyenphansu1802 <nguyenphansu1802@gmail.com>
2022-07-13 21:15:16 +0700

    update137

--- entity_model/base.py
+++ entity_model/base.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # engine = create_engine('mysql://root:nguyenanhquoc1@localhost:3306/distracted_driver')
-engine = create_engine('mysql://root:nguyenanhquoc1@localhost:3306/distracted_driver', 
- pool_size=20, max_overflow=0, pool_recycle=60*60)
-# engine = create_engine('mysql://root:123456@localhost:3306/distracted_driver2', pool_size=20, max_overflow=0, pool_recycle=60*60)
+# engine = create_engine('mysql://root:nguyenanhquoc1@localhost:3306/distracted_driver', 
+#  pool_size=20, max_overflow=0, pool_recycle=60*60)
+engine = create_engine('mysql://root:123456@localhost:3306/distracted_driver2', pool_size=20, max_overflow=0, pool_recycle=60*60)
 try:
     engine.connect()
     print('Connect successfully')
